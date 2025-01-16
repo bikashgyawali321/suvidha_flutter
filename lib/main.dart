9f3d9db8a16444c639174e71db2802e3856f6c1f
Bikash Gyawali <gyawalibikash321@gmail.com>
2025-01-16 09:50:39 +0545

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -13,7 +14,8 @@ import 'services/auth_service.dart';
 import 'services/custom_hive.dart';
 
 void main() async {
-      
+  WidgetsFlutterBinding.ensureInitialized();
+
   await CustomHive().init();
   runApp(ProviderWrappedApp());
 }
